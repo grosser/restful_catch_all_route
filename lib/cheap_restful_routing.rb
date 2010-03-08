29cd74e22a1699576df215afe0b5c875f58594a8
grosser <grosser.michael@gmail.com>
2010-03-08 08:34:14 +0100

    initial

---
+++ lib/cheap_restful_routing.rb
@@ -0,0 +1,10 @@
+class ActionController::Routing::RouteSet::Mapper
+  def cheap_restful_routing(options={})
+    connect ':controller/:id/:action', options.merge(:action => :show)
+    connect ':controller/:id', :action => :show, :conditions => { :method => :get }
+    connect ':controller/:id', :action => :update, :conditions => { :method => :put }
+    connect ':controller/:id', :action => :destroy, :conditions => { :method => :delete }
+    connect ':controller', :action => :index, :conditions => { :method => :get }
+    connect ':controller', :action => :create, :conditions => { :method => :post }
+  end
+end
